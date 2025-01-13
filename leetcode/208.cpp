ece67c9d62f2c832a6a0b9e9ec62c06297f24e82
Vivek Mittal <vivek.mittal@redbus.com>
2025-01-13 07:53:07 +0530

    update

---
+++ leetcode/208.cpp
@@ -0,0 +1,50 @@
+#include "main.h"
+
+struct TrieNode {
+	std::vector<TrieNode* > characters;
+	bool flag;
+
+    TrieNode() {
+        characters.resize(26);
+        flag = false;
+    }
+};
+
+class Trie {
+	private:
+		TrieNode *root;
+	public:
+		Trie () {
+			this->root = new TrieNode();
+		}
+		
+		void insert(std::string word) {
+			
+			TrieNode *node = this->root;
+			for (auto &c : word) {
+				if (node->characters[c-'a'] == nullptr) {
+					TrieNode *t = new TrieNode();
+					node->characters[c-'a'] = t;
+				}
+				node = node->characters[c-'a'];
+			}
+			node->flag = true;
+		}
+		
+		bool search(std::string word, bool checkForPrefix = false) {
+			
+			TrieNode *node = root;
+			for (auto &c : word) {
+				if (node->characters[c-'a'] == nullptr) {
+					return false;
+				}
+				node = node->characters[c-'a'];
+			}
+			
+			return checkForPrefix ? true : node->flag;
+		}
+		
+		bool startsWith(std::string prefix) {
+			return this->search(prefix, true);
+		}
+};
