9b3ae9a02f14eced7f97f903ede04e59efda4b11
vivekm92 <viv.mttl@gmail.com>
2025-01-21 00:39:20 +0530

    [cses]: add solution

--- cses/1070.cpp
+++ cses/1070.cpp
@@ -2,9 +2,27 @@
 
 using namespace std;
 
+void solve(int n) {
+    if (n <= 3) {
+        cout << "NO SOLUTION" << "\n";
+    }
+    
+    vector<int> arr;
+    for (int i = 1; i <= n; i+=2) {
+        cout << i << " ";
+    }
+    for (int i =2; i <= n; i+=2) {
+        cout << i << " ";
+    }
+    cout << "\n";
+}
 
 int main() {
-
+    
+    int n;
+    cin >> n;
+    
+    solve(n);
 	
 	return 0;
 }
---
+++ cses/README.md
@@ -0,0 +1,8 @@
+
+
+## Introductory Problems
+
+* [Weird Algorithm](https://cses.fi/problemset/task/1068)&emsp;[solution](1068.cpp)
+* [Missing Number](https://cses.fi/problemset/task/1083)&emsp;[solution](1083.cpp)
+* [Repetitions](https://cses.fi/problemset/task/1069)&emsp;[solution](1069.cpp)
+* [Increasing Array](https://cses.fi/problemset/task/1094)&emsp;[solution](1094.cpp)
