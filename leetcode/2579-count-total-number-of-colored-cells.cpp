47384a055db499c0f07b1e7dd34e7185471da194
vivekm92 <viv.mttl@gmail.com>
2025-03-05 11:02:40 +0530

    [lc]: add 2579-count-total-number-of-colored-cells.cpp

---
+++ leetcode/2579-count-total-number-of-colored-cells.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+private:
+    long long solveColoredCells(long long n) {
+        return 2 * n * n - (2 * n - 1);
+    }
+public:
+    long long coloredCells(int n) {
+        return solveColoredCells(n);
+    }
+};
+
+// Driver Code for testing
+int main() {
+    Solution *sol = new Solution();
+    cout << sol->coloredCells(2) << "\n";
+    cout << sol->coloredCells(3) << "\n";
+    return 0;
+}
