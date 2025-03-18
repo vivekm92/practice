d949441be14be6018c62404140e41c1de2ddcea5
vivekm92 <viv.mttl@gmail.com>
2025-03-18 10:44:04 +0530

    [lc]: add 2206-divide-array-into-equal-pairs.cpp

--- leetcode/2206-divide-array-into-equal-pairs.cpp
+++ leetcode/2206-divide-array-into-equal-pairs.cpp
@@ -39,14 +39,50 @@ private:
 
         return true;
     }
+
+    // Approach 3
+    // using constant arr
+    // T(n) : O(n) ; S(n) : O(n)
+    bool solveDivideArray2(vector<int>& nums) {
+
+        int maxNum = *max_element(nums.begin(), nums.end());
+        vector<bool> arr(maxNum+1, false);
+        for (auto &num : nums) {
+            arr[num] = arr[num] ^ true;
+        }
+
+        for (auto &num : nums) {
+            if (arr[num] == true) return false;
+        }
+
+        return true;
+    }
+
+    // Approach 4
+    // using hash-set
+    // T(n) : O(n) ; S(n) : O(n)
+    bool solveDivideArray3(vector<int> &nums) {
+
+        unordered_set<int> us;
+        for (auto &num : nums) {
+            if (us.count(num) > 0) us.erase(num);
+            else us.insert(num);
+        }
+
+        return us.size() == 0;
+    }
 public:
     bool divideArray(vector<int>& nums) {
-        return solveDivideArray(nums);
+        return solveDivideArray2(nums);
     }
 };
 
 // Driver Code for testing
 int main() {
+
+    vector<int> arr = {3,2,3,2,2,2};
+    Solution *sol = new Solution();
+    cout << sol->divideArray(arr) << "\n";
 
     return 0;
 }
