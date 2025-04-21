0696aea32d4860ae215835325f6f103e20b88e8a
vivekm92 <viv.mttl@gmail.com>
2025-04-22 04:37:34 +0530

    [lc]: add 0779-k-th-symbol-in-grammar.cpp

--- leetcode/0779-k-th-symbol-in-grammar.cpp
+++ leetcode/0779-k-th-symbol-in-grammar.cpp
@@ -5,17 +5,40 @@ using namespace std;
 // https://leetcode.com/problems/k-th-symbol-in-grammar/description/
 class Solution {
 private:
+    // Approach 1
+    // using recursion
+    // T(n) : O(n) ; S(n) : O(n)
     int solveKthGrammar(int n, int k) {
-        
+        if (k == 2) {
+            return 1;
+        } else if (n == 1 || k == 1) {
+            return 0;
+        }
+
+        int x = pow(2, n-1);
+        if (k > x / 2) {
+            if (n%2 != 0) return solveKthGrammar(n-1, x-k+1);
+            else return solveKthGrammar(n-1, x-k+1) ^ 1;
+        }
+
+        return solveKthGrammar(n-1, k);
+    }
+
+    // Approach 2
+    // using math
+    // T(n) : O(logn) ; S(n) : O(1)
+    int solveKthGrammar1(int n, int k) {
+        int count = __builtin_popcount(k-1);
+        return count % 2 == 0 ? 0 : 1;
     }
 public:
     int kthGrammar(int n, int k) {
-        return solveKthGrammar(n, k);
+        return solveKthGrammar1(n, k);
     }
 };
 
 // Driver Code for testing
 int main() {
-        
+
     return 0;
 }
