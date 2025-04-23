dfe1bf69491b2e9f92d6b7145655d7c0b14295fc
vivekm92 <viv.mttl@gmail.com>
2025-04-23 23:14:34 +0530

    [lc]: add 0217-contains-duplicate.cpp

--- leetcode/0217-contains-duplicate.cpp
+++ leetcode/0217-contains-duplicate.cpp
@@ -7,19 +7,33 @@ using namespace std;
 // https://leetcode.com/problems/contains-duplicate/description/
 class Solution {
 private:
+    // Approach 1
     // using hash-set
     // T(n) : O(n) ; S(n) : O(n)
      bool solvecontainsDuplicate(vector<int>& nums) {
          unordered_set<int> us;
-         for (auto n : nums) {
+         for (auto &n : nums) {
              if (us.find(n) != us.end()) return true;
              us.insert(n);
          }
          return false;
      }
+
+     // Approach 2
+     // using sorting
+     // T(n) : O(nlogn) ; S(n) : O(1)
+    bool solvecontainsDuplicate1(vector<int> &nums) {
+
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i-1]) return true;
+        }
+        return false;
+    }
 public:
     bool containsDuplicate(vector<int>& nums) {
-        return solvecontainsDuplicate(nums);
+        return solvecontainsDuplicate1(nums);
     }
 };
 
